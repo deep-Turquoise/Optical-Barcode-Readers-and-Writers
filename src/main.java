c2957335a6b4260f01196f3f06a8aa5338089909
deep-Turquoise <nicholasnelson10@gmail.com>
2017-05-18 10:29:37 -0700

    more setup

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
+// Nicholas Nelson, David Henderson, Christopher Calderon
+// Nathan Mauga, Haley Dimapilis
+// CST338-30_SU17: Software Design
+// Module 4
+// (M4) Write a Java program (Optical Barcode Readers) (4 hrs)
 
 public class main
 {  
