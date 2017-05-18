4682f2ed2242e80870b3d3c55653f16074f93ed8
deep-Turquoise <nicholasnelson10@gmail.com>
2017-05-18 10:27:44 -0700

    barcodeIO setup

--- src/BarcodeIO.java
+++ src/BarcodeIO.java
@@ -1,5 +1,33 @@
 
 public class BarcodeIO
 {
+    public boolean scan(BarcodeImage bc)
+    {
 
+    }
+
+    public boolean readText(String text)
+    {
+        
+    }
+
+    public boolean generateImageFromText()
+    {
+        
+    }
+
+    public boolean translateImageToText()
+    {
+        
+    }
+
+    public void displayTextToConsole()
+    {
+        
+    }
+    
+    public void displayImageToConsole()
+    {
+        
+    }
 }
