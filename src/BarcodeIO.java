af03e93313d201c65da8eb10a65f3bc199e55a82
cayuse <cayuse@deepbondi.net>
2017-05-19 12:41:29 -0700

    makes barcode an interface. and ignores binary .class files

--- src/BarcodeIO.java
+++ src/BarcodeIO.java
@@ -1,33 +1,15 @@
 
-public class BarcodeIO
+public interface BarcodeIO
 {
-    public boolean scan(BarcodeImage bc)
-    {
+   public boolean scan(BarcodeImage bc);
 
-    }
+   public boolean readText(String text);
 
-    public boolean readText(String text)
-    {
-        
-    }
+   public boolean generateImageFromText();
 
-    public boolean generateImageFromText()
-    {
-        
-    }
+   public boolean translateImageToText();
 
-    public boolean translateImageToText()
-    {
-        
-    }
+   public void displayTextToConsole();
 
-    public void displayTextToConsole()
-    {
-        
-    }
-    
-    public void displayImageToConsole()
-    {
-        
-    }
+   public void displayImageToConsole();
 }
