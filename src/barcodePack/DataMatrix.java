e56dde9a0fa5964af831f475e306eb0bd471a0f9
deep-Turquoise <nicholasnelson10@gmail.com>
2017-05-23 12:12:02 -0700

    initial work on data matrix

--- src/barcodePack/DataMatrix.java
+++ src/barcodePack/DataMatrix.java
@@ -13,31 +13,32 @@ public class DataMatrix implements BarcodeIO
    public DataMatrix()
    {
        image = new BarcodeImage();
-       actualWidth = 0;
-       actualHeight = 0;
        text = "undefined";
    }
    
-   public DataMatrix(BarcodeImage bc)
+   public DataMatrix(BarcodeImage giv)
    {
-      image = bc;
-      // TO-DO:  Call scan() and avoid duplication of code here.
+      scan(giv);
    }
 
    public DataMatrix(String giv)
    {
-       this();
-       
+       image = new BarcodeImage();
+       readText(giv);
    }
    
-   public boolean scan(BarcodeImage bc)
+   public boolean scan(BarcodeImage giv)
    {
+      image = (BarcodeImage)giv.clone();
+      cleanImage();
       return false;
    }
 
-   public boolean readText(String text)
+   public boolean readText(String giv)
    {
-      return false;
+      // i'm really not sure what this for
+      text = giv;
+      return true;
    }
 
    public boolean generateImageFromText()
@@ -59,4 +60,97 @@ public class DataMatrix implements BarcodeIO
    {
       
    }
+   
+   private void findTopLeftCoords(int xTopLeft, int yTopLeft)
+   {
+      for(int y = 0; y < image.MAX_HEIGHT; ++y)
+      {
+         for(int x = 0; x < image.MAX_WIDTH; ++x)
+         {
+            if(image.getPixel(y, x) == true)
+            {
+               xTopLeft = x;
+               yTopLeft = y;
+               break;
+            }
+         }
+      }
+   }
+   
+   private void determineHeight(int xTopLeft, int yTopLeft)
+   {
+      for(int y = yTopLeft; y < image.MAX_HEIGHT; ++y)
+      {
+         if(image.getPixel(y, xTopLeft) == true)
+         {
+            actualHeight = y - yTopLeft;
+         }
+         else
+         {
+            break;
+         }
+      }
+   }
+   
+   private void determineWidth(int xTopLeft, int yTopLeft)
+   {
+      for(int x = xTopLeft; x < image.MAX_WIDTH; ++x)
+      {
+         if(image.getPixel(yTopLeft+actualHeight, x) == true)
+         {
+            actualWidth = x- xTopLeft;
+         }
+         else
+         {
+            break;
+         }
+      }
+   }
+   
+   private void shiftDown(int xTopLeft, int yTopLeft)
+   {
+      //find bottom y coord
+      for(int y = image.MAX_HEIGHT-1; y >= 0; --y)
+      {
+         for(int x = 0; x < image.MAX_WIDTH; ++x)
+         {
+            // sw
+         }
+      }
+   }
+   
+   private void cleanImage()
+   {
+      int xTopLeft = 0;
+      int yTopLeft = 0;
+      findTopLeftCoords(xTopLeft, yTopLeft);
+      
+      determineWidth(xTopLeft, yTopLeft);
+      determineHeight(xTopLeft, yTopLeft);
+      shiftDown(xTopLeft, yTopLeft);
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
