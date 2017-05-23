7c084a899f8f11b00772b818e8804589b245e2d1
deep-Turquoise <nicholasnelson10@gmail.com>
2017-05-22 17:02:46 -0700

    began work on matrix

--- src/barcodePack/DataMatrix.java
+++ src/barcodePack/DataMatrix.java
@@ -5,15 +5,29 @@ public class DataMatrix implements BarcodeIO
 {
    public static final char BLACK_CHAR = '*';
    public static final char WHITE_CHAR = ' ';
-   
+   private BarcodeImage image;
+   private String text;
+   private int actualWidth;
+   private int actualHeight;
+
    public DataMatrix()
    {
-      
+       image = new BarcodeImage();
+       actualWidth = 0;
+       actualHeight = 0;
+       text = "undefined";
    }
    
    public DataMatrix(BarcodeImage bc)
    {
-      
+      image = bc;
+      // TO-DO:  Call scan() and avoid duplication of code here.
+   }
+
+   public DataMatrix(String giv)
+   {
+       this();
+       
    }
    
    public boolean scan(BarcodeImage bc)
